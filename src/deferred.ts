25892eac3ed2d1c759dfcf5885011d54c313d89e
django <six.django@gmail.com>
2024-03-26 15:47:24 +0800

    feat: upgrade dependency

--- src/deferred.ts
+++ src/deferred.ts
@@ -1,14 +1,20 @@
 export class Deferred<T = any> {
   promise: Promise<T>
-
+  status: 'pending' | 'resolved' | 'rejected' = 'pending'
   resolve!: (value: T | PromiseLike<T>) => void
 
   reject!: (reason?: any) => void
 
   constructor() {
     this.promise = new Promise((resolve, reject) => {
-      this.resolve = resolve
-      this.reject = reject
+      this.resolve = (args) => {
+        this.status = 'resolved'
+        resolve(args)
+      }
+      this.reject = (args) => {
+        this.status = 'rejected'
+        reject(args)
+      }
     })
   }
 }
